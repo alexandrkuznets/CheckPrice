5d308fd27fbf612393b700bbe039c02007e75650
Aleksander Kuznetsov <kosogor2709@mail.ru>
2026-05-28 18:22:20 +0300

    new column in table users(is_active)

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
 from app.database.db_config import Base
@@ -8,5 +8,6 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True)
     password = Column(String)
+    is_active = Column(Boolean, default=False)
 
     products = relationship("Product", back_populates="user", cascade="all, delete-orphan")
