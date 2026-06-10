8ed6543e60e171c7d13dd108817f59a573b1fb1d
Aleksander Kuznetsov <kosogor2709@mail.ru>
2026-06-10 22:38:36 +0300

    added error handling

--- app/client/wb.py
+++ app/client/wb.py
@@ -1,24 +1,25 @@
 from curl_cffi import requests
-from app.config import settings
 
+from app.config import settings
 
 session = requests.Session(impersonate="chrome120")
 session.headers.update({'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Host": "www.wildberries.ru",
-        "Pragma": "no-cache",
-        "Priority": "u=0, i",
-        "Referer": "https://www.google.com/",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "cross-site",
-        "TE": "trailers",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:151.0) Gecko/20100101 Firefox/151.0"})
+                        'Content-Type': 'application/json',
+                        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                        "Cache-Control": "no-cache",
+                        "Connection": "keep-alive",
+                        "Host": "www.wildberries.ru",
+                        "Pragma": "no-cache",
+                        "Priority": "u=0, i",
+                        "Referer": "https://www.google.com/",
+                        "Sec-Fetch-Dest": "document",
+                        "Sec-Fetch-Mode": "navigate",
+                        "Sec-Fetch-Site": "cross-site",
+                        "TE": "trailers",
+                        "Upgrade-Insecure-Requests": "1",
+                        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:151.0) Gecko/20100101 Firefox/151.0"})
 session.cookies.update(settings.cookie_wb)
+
 
 def get_wb_card_data(article):
     url = "https://www.wildberries.ru/__internal/u-card/cards/v4/detail"
@@ -30,16 +31,24 @@ def get_wb_card_data(article):
         "lang": "ru",
         "dest": "1259570991"
     }
-    response = session.get(
-        url,
-        params=params,
-        timeout=10
-    )
-    a = response.json()
-    product = a['products'][0]
-    price_kopecks = product['sizes'][0]['price']['product']
-    price_rub = price_kopecks / 100
-    return price_rub
+    try:
+        response = session.get(
+            url,
+            params=params,
+            timeout=10
+        )
 
-
-
+        if response.status_code == 200:
+            result = response.json()
+            product = result['products'][0]
+            price_kopecks = product['sizes'][0]['price']['product']
+            price_rub = price_kopecks / 100
+            return price_rub
+        else:
+            print("Отправляем письмо хозяину")
+    except (KeyError, IndexError) as ex:
+        print("Отправляем письмо хозяину")
+        return None
+    except (requests.RequestsError, ConnectionError, TimeoutError) as ex:
+        print("Отправляем письмо хозяину")
+        return None
--- app/services/products_sync.py
+++ app/services/products_sync.py
@@ -1,19 +1,30 @@
+from sqlite3 import SQLITE_ROW
+
 from sqlalchemy import select, update
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.database.db_config import Session
 from app.models.product import Product
 
 
 def get_products():
-    with Session() as session:
-        result = session.execute(
-            select(Product.user_id, Product.product_url, Product.last_price, Product.desired_price))
-        return [row for row in result.fetchall()]
+    try:
+        with Session() as session:
+            result = session.execute(
+                select(Product.user_id, Product.product_url, Product.last_price, Product.desired_price))
+            return [row for row in result.fetchall()]
+    except SQLAlchemyError as ex:
+        # ОТправляем письмо хозяину с ощибкой ex
+        return []
 
 
 def update_product(article, new_price):
-    with Session() as session:
-        result = session.execute(select(Product).where(Product.product_url == article))
-        product = result.scalar_one_or_none()
-        product.last_price = new_price
-        session.commit()
+    try:
+        with Session() as session:
+            result = session.execute(select(Product).where(Product.product_url == article))
+            product = result.scalar_one_or_none()
+            product.last_price = new_price
+            session.commit()
+    except SQLAlchemyError as ex:
+        # ОТправляем письмо хозяину с ощибкой ex
+        pass
