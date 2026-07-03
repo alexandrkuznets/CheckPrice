9e1335857665cce62b77eac9e56fbe896a23309c
Aleksander Kuznetsov <kosogor2709@mail.ru>
2026-07-03 23:09:04 +0300

    change wb headers

--- app/client/wb.py
+++ app/client/wb.py
@@ -5,33 +5,42 @@ from app.celery_app import logger
 from app.services.email import send_email_to_host
 
 session = requests.Session(impersonate="chrome120")
-session.headers.update({'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-                        "Cache-Control": "no-cache",
-                        "Connection": "keep-alive",
-                        "Host": "www.wildberries.ru",
-                        "Pragma": "no-cache",
-                        "Priority": "u=0, i",
-                        "Referer": "https://www.google.com/",
-                        "Sec-Fetch-Dest": "document",
-                        "Sec-Fetch-Mode": "navigate",
-                        "Sec-Fetch-Site": "cross-site",
-                        "TE": "trailers",
-                        "Upgrade-Insecure-Requests": "1",
-                        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:151.0) Gecko/20100101 Firefox/151.0"})
+session.headers.update({
+"Accept": "*/*",
+"Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+"Cache-Control": "no-cache",
+"Connection": "keep-alive",
+"deviceid": "site_c0f729feace542a6ade803f312edc6d9",
+"Host": "www.wildberries.ru",
+"Pragma": "o-cache",
+"Priority": "u=4",
+"Referer": "https://www.wildberries.ru/",
+"Sec-Fetch-Dest": "empty",
+"Sec-Fetch-Mode": "cors",
+"Sec-Fetch-Site": "same-origin",
+"TE": "trailers",
+"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:152.0) Gecko/20100101 Firefox/152.0",
+"x-requested-with": "XMLHttpRequest",
+"x-spa-version": "14.16.1"
+})
 session.cookies.update(settings.cookie_wb)
 
 
 def get_wb_product_data(article):
     url = "https://www.wildberries.ru/__internal/u-card/cards/v4/detail"
 
+
     params = {
         "nm": int(article),
         "appType": "1",
         "curr": "rub",
         "lang": "ru",
-        "dest": "1259570991"
+        "dest": "-1257786",
+        "ab_testing": "false",
+        "mtype": "257",
+        "hide_dtype": "15",
+        "hide_vflags": "4294967296",
+        "spp": "30"
     }
     try:
         response = session.get(
